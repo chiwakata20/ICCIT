fdbc05b8eb067658f63a18923e4827552c527342
kevin chiwakata <chiwakatakevin@gmail.com>
2026-07-20 09:43:51 -0700

    Fix model import filename casing for Vercel

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Subject, validateSubject } = require("../models/Subject");
+const { Subject, validateSubject } = require("../models/subject");
 const { Board } = require("../models/Board");
 
 router.get("/", async (req, res) => {
